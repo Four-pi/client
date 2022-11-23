c5b7f371f9df631c39b574d300776dbe93092c5a
Hepheir <hepheir@gmail.com>
2022-11-23 16:11:25 +0900

    test: 11/21에 학교에서 시행한 nmap 스캔 데이터를 목업에 추가

--- src/apis/mock-data.ts
+++ src/apis/mock-data.ts
@@ -153,6 +153,377 @@ export const mockRequests: Request[] = [
 
 export const mockReports: Report[] = [
     {
+        id: "0",
+        ip: "localhost",
+        subnet_mask: "192.168.13.0",
+        created_at: "2022-11-21",
+        ip_reports: [
+            {
+                ip: "192.168.13.116",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.121",
+                os: "",
+                port_reports: [
+                    {
+                        port: "135",
+                        protocol: "tcp",
+                        service: "msrpc",
+                        version: "Microsoft Windows RPC"
+                    },
+                    {
+                        port: "139",
+                        protocol: "tcp",
+                        service: "netbios-ssn",
+                        version: "Microsoft Windows netbios-ssn"
+                    },
+                    {
+                        port: "443",
+                        protocol: "tcp",
+                        service: "ssl/https",
+                        version: "VMware Workstation SOAP API 16.1.0"
+                    },
+                    {
+                        port: "903",
+                        protocol: "tcp",
+                        service: "ssl/vmware-auth",
+                        version: "VMware Authentication Daemon 1.10 (Uses VNC, SOAP)"
+                    },
+                    {
+                        port: "1024",
+                        protocol: "tcp",
+                        service: "msrpc",
+                        version: "Microsoft Windows RPC"
+                    },
+                    {
+                        port: "1026",
+                        protocol: "tcp",
+                        service: "msrpc",
+                        version: "Microsoft Windows RPC"
+                    },
+                    {
+                        port: "3389",
+                        protocol: "tcp",
+                        service: "ms-wbt-server",
+                        version: "Microsoft Terminal Services"
+                    },
+                    {
+                        port: "5357",
+                        protocol: "tcp",
+                        service: "http",
+                        version: "Microsoft HTTPAPI httpd 2.0 (SSDP/UPnP)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.129",
+                os: "",
+                port_reports: [
+                    {
+                        port: "80",
+                        protocol: "tcp",
+                        service: "soap",
+                        version: "gSOAP 2.7"
+                    },
+                    {
+                        port: "631",
+                        protocol: "tcp",
+                        service: "soap",
+                        version: "gSOAP 2.7"
+                    },
+                    {
+                        port: "8080",
+                        protocol: "tcp",
+                        service: "soap",
+                        version: "gSOAP 2.7"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.170",
+                os: "",
+                port_reports: [
+                    {
+                        port: "5357",
+                        protocol: "tcp",
+                        service: "http",
+                        version: "Microsoft HTTPAPI httpd 2.0 (SSDP/UPnP)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.172",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.174",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.175",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.176",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.177",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.179",
+                os: "",
+                port_reports: [
+                    {
+                        port: "5000",
+                        protocol: "tcp",
+                        service: "rtsp",
+                        version: "AirTunes rtspd 665.13.1"
+                    },
+                    {
+                        port: "7000",
+                        protocol: "tcp",
+                        service: "rtsp",
+                        version: "AirTunes rtspd 665.13.1"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.181",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.182",
+                os: "",
+                port_reports: [
+                    {
+                        port: "135",
+                        protocol: "tcp",
+                        service: "msrpc",
+                        version: "Microsoft Windows RPC"
+                    },
+                    {
+                        port: "139",
+                        protocol: "tcp",
+                        service: "netbios-ssn",
+                        version: "Microsoft Windows netbios-ssn"
+                    },
+                    {
+                        port: "3306",
+                        protocol: "tcp",
+                        service: "mysql",
+                        version: "MySQL (unauthorized)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.183",
+                os: "",
+                port_reports: [
+                    {
+                        port: "135",
+                        protocol: "tcp",
+                        service: "msrpc",
+                        version: "Microsoft Windows RPC"
+                    },
+                    {
+                        port: "139",
+                        protocol: "tcp",
+                        service: "netbios-ssn",
+                        version: "Microsoft Windows netbios-ssn"
+                    },
+                    {
+                        port: "902",
+                        protocol: "tcp",
+                        service: "ssl/vmware-auth",
+                        version: "VMware Authentication Daemon 1.10 (Uses VNC, SOAP)"
+                    },
+                    {
+                        port: "912",
+                        protocol: "tcp",
+                        service: "vmware-auth",
+                        version: "VMware Authentication Daemon 1.0 (Uses VNC, SOAP)"
+                    },
+                    {
+                        port: "5357",
+                        protocol: "tcp",
+                        service: "http",
+                        version: "Microsoft HTTPAPI httpd 2.0 (SSDP/UPnP)"
+                    },
+                    {
+                        port: "5800",
+                        protocol: "tcp",
+                        service: "vnc-http",
+                        version: "TightVNC (user: desktop-pap9t6v; VNC TCP port: 5900)"
+                    },
+                    {
+                        port: "5900",
+                        protocol: "tcp",
+                        service: "vnc",
+                        version: "VNC (protocol 3.8)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.185",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.187",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.190",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.191",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.193",
+                os: "",
+                port_reports: [
+                    {
+                        port: "903",
+                        protocol: "tcp",
+                        service: "ssl/vmware-auth",
+                        version: "VMware Authentication Daemon 1.10 (Uses VNC, SOAP)"
+                    },
+                    {
+                        port: "5357",
+                        protocol: "tcp",
+                        service: "http",
+                        version: "Microsoft HTTPAPI httpd 2.0 (SSDP/UPnP)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.194",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.195",
+                os: "",
+                port_reports: [
+                    {
+                        port: "3306",
+                        protocol: "tcp",
+                        service: "mysql",
+                        version: "MySQL (unauthorized)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.196",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.198",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.199",
+                os: "",
+                port_reports: []
+            },
+            {
+                ip: "192.168.13.1",
+                os: "",
+                port_reports: [
+                    {
+                        port: "22",
+                        protocol: "tcp",
+                        service: "ssh",
+                        version: "Cisco SSH 1.25 (protocol 1.99)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.233",
+                os: "",
+                port_reports: [
+                    {
+                        port: "22",
+                        protocol: "tcp",
+                        service: "ssh",
+                        version: "OpenSSH 7.6p1 Ubuntu 4ubuntu0.7 (Ubuntu Linux; protocol 2.0)"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.253",
+                os: "",
+                port_reports: [
+                    {
+                        port: "22",
+                        protocol: "tcp",
+                        service: "ssh",
+                        version: "Cisco SSH 1.25 (protocol 1.99)"
+                    },
+                    {
+                        port: "23",
+                        protocol: "tcp",
+                        service: "telnet",
+                        version: "Cisco router telnetd"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.2",
+                os: "",
+                port_reports: [
+                    {
+                        port: "22",
+                        protocol: "tcp",
+                        service: "ssh",
+                        version: "(protocol 2.0)"
+                    },
+                    {
+                        port: "443",
+                        protocol: "tcp",
+                        service: "ssl/http",
+                        version: "Cisco Wireless LAN Controller httpd"
+                    },
+                    {
+                        port: "16080",
+                        protocol: "tcp",
+                        service: "http",
+                        version: "OpenResty web app server 1.7.10.1"
+                    }
+                ]
+            },
+            {
+                ip: "192.168.13.3",
+                os: "",
+                port_reports: [
+                    {
+                        port: "22",
+                        protocol: "tcp",
+                        service: "ssh",
+                        version: "OpenSSH 7.8 (protocol 2.0)"
+                    }
+                ]
+            }
+        ]
+    },
+    {
         id: '1',
         ip: 'localhost',
         subnet_mask: '24',
