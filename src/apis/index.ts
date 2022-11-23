77eedbe673d7ea9b5b4aa570b4b8710195d6eb33
Hepheir <hepheir@gmail.com>
2022-11-23 16:23:50 +0900

    test: 일부 api에 rest api 연결

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,18 @@
 import { addLoggerProxy } from "../system/logger";
 import type * as api from "./base";
 import * as mock from "./mock";
-import { UserAPI } from "./session-storage/user";
+import * as rest from "./rest";
 import { PortAPI } from "./session-storage/port";
 
-export const reportApi: api.ReportAPI = addLoggerProxy(new mock.ReportAPI());
-export const requestApi: api.RequestAPI = addLoggerProxy(new mock.RequestAPI());
-export const scanApi: api.ScanAPI = addLoggerProxy(new mock.ScanAPI());
-export const userApi: api.UserAPI = addLoggerProxy(new UserAPI());
-export const machineStatusApi: api.MachineStatusAPI = addLoggerProxy(new mock.MachineStatusAPI());
-export const portApi: api.PortAPI = addLoggerProxy(new PortAPI());
+function loggerApplier<T extends Object>(x: T, enable: boolean): T {
+    return enable ? addLoggerProxy(x) : x;
+}
+
+const useLogger: boolean = false;
+
+export const reportApi: api.ReportAPI = loggerApplier(new mock.ReportAPI(), useLogger);
+export const requestApi: api.RequestAPI = loggerApplier(new mock.RequestAPI(), useLogger);
+export const scanApi: api.ScanAPI = loggerApplier(new mock.ScanAPI(), useLogger);
+export const userApi: api.UserAPI = loggerApplier(new rest.UserAPI(), useLogger);
+export const machineStatusApi: api.MachineStatusAPI = loggerApplier(new rest.MachineStatusAPI(), useLogger);
+export const portApi: api.PortAPI = loggerApplier(new PortAPI(), useLogger);
