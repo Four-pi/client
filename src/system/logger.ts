a9757c63381061e66230d39ef6db532c65d466f6
Hepheir <hepheir@gmail.com>
2022-11-24 17:20:14 +0900

    chore: 사용되지 않는 코드 제거

--- src/system/logger.ts
+++
@@ -1,9 +0,0 @@
-export function addLoggerProxy<T extends Object>(target: T): T {
-    const handler: ProxyHandler<T> = {
-        get(target, prop, receiver) {
-            console.log('[API]', target.constructor.name, '/', prop, );
-            return Reflect.get(target, prop, receiver);
-        }
-    }
-    return new Proxy(target, handler);
-}
